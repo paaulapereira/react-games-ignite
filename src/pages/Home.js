c09647095bfbe7495f1daa4dd5502fcba213cdb3
paula_pereira <paula.decosta@alumnos.uneatlantico.es>
2021-11-26 15:44:17 +0100

    estilos basicos

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,20 +1,56 @@
 import React, { useEffect } from "react";
 //Redux
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { loadGames } from "../actions/gamesAction";
+//components
+import Game from "../components/Game";
+//Styling and animation
+import styled from "styled-components";
+import { motion } from "framer-motion";
 
 const Home = () => {
   //FETCH GAMES
   const dispatch = useDispatch();
-  //el useEffect se ejecutará cuando el app cargue (renders)
+  //el useEffect se ejecutará cuando el dispatch happends
   useEffect(() => {
     dispatch(loadGames());
-  });
+  }, [dispatch]);
+  //get that data back - > creamos una constante para cada uno de los tipos de juegos
+  const { popular, newGames, upcoming } = useSelector((state) => state.games);
+
   return (
-    <div>
-      <h1>HOME</h1>
-    </div>
+    <GameList>
+      <h2>Upcoming Games</h2>
+      <Games>
+        {upcoming.map((game) => (
+          <Game
+            name={game.name}
+            released={game.released}
+            id={game.id}
+            image={game.background_image}
+            key={game.id}
+          />
+        ))}
+      </Games>
+    </GameList>
   );
 };
 
+const GameList = styled(motion.div)`
+  padding: 0rem 5rem;
+  h2 {
+    padding: 5rem 0rem;
+  }
+`;
+
+const Games = styled(motion.div)`
+  min-height: 80vh;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(500px, 1fr));
+  grid-column-gap: 3rem;
+  grid-row-gap: 5rem;
+`;
+
 export default Home;
+
+//grid-template-columns: repeat(auto-fit, minmax(500px, 1fr)) significa que las columnas del grid se van a repetir el maximo de veces posible hasta que entren, y que hagan autofit con la pagina, y que el minimo tamaño es 500px, pero si no llega a ese tamaÑo se expande el maximo dque pueda
