a6fbf58b63af066b7815c7efcc0a7d03363dd634
paula_pereira <paula.decosta@alumnos.uneatlantico.es>
2021-11-30 12:59:34 +0100

    nav

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import { React } from "react";
 //Components and pages
 import Home from "./pages/Home";
+import Nav from "./components/Nav";
+//styles
 import GlobalStyles from "./components/GlobalStyles";
+//router
 import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyles />
+      <Nav />
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/game/:id" element={<Home />}></Route>
