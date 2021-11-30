b499eab6877d506b3f34d5a902fea573634953d5
paula_pereira <paula.decosta@alumnos.uneatlantico.es>
2021-11-30 08:53:59 +0100

    styled detail

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,7 +2,7 @@ import { combineReducers } from "redux";
 import gamesReducer from "./gamesReducer";
 import detailReducer from "./detailReducer";
 
-//creamos una funcion que juntara todos los reducers en uno
+//creamos una funcion que juntara todos los reducers en uno (los states)
 const rootReducer = combineReducers({
   games: gamesReducer,
   detail: detailReducer,
